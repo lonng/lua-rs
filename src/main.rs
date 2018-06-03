a244032e660d3445dc7d62f448dc78d235e9369b
Lonng <chris@lonng.org>
2018-06-03 18:23:55 +0800

    lexical: add lua-tests

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use lua::state::State;
 
 fn main() {
     let mut state = State::new();
-    state.load_string(r"hello world 12344444.011521 '\x45' '\50'").unwrap();
+//    state.load_string(r"hello world 12344444.011521 '\x45' '\50'").unwrap();
+    state.load_file("lua-tests/api.lua").unwrap();
 }
