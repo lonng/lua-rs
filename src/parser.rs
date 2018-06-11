7ca87646341b71d964e63760b752204b35050c93
Lonng <chris@lonng.org>
2018-06-11 17:13:09 +0800

    BNF

--- src/parser.rs
+++ src/parser.rs
@@ -67,6 +67,67 @@ impl<R: Read> Parser<R> {
         }
     }
 
+
+    /// Here is the complete syntax of Lua in extended BNF. (It does not describe operator precedences.)
+    ///
+    /// ```BNF
+    ///	chunk ::= {stat [`;´]} [laststat [`;´]]
+    ///
+    ///	block ::= chunk
+    ///
+    ///	stat ::=  varlist `=´ explist |
+    ///		 functioncall |
+    ///		 do block end |
+    ///		 while exp do block end |
+    ///		 repeat block until exp |
+    ///		 if exp then block {elseif exp then block} [else block] end |
+    ///		 for Name `=´ exp `,´ exp [`,´ exp] do block end |
+    ///		 for namelist in explist do block end |
+    ///		 function funcname funcbody |
+    ///		 local function Name funcbody |
+    ///		 local namelist [`=´ explist]
+    ///
+    ///	laststat ::= return [explist] | break
+    ///
+    ///	funcname ::= Name {`.´ Name} [`:´ Name]
+    ///
+    ///	varlist ::= var {`,´ var}
+    ///
+    ///	var ::=  Name | prefixexp `[´ exp `]´ | prefixexp `.´ Name
+    ///
+    ///	namelist ::= Name {`,´ Name}
+    ///
+    ///	explist ::= {exp `,´} exp
+    ///
+    ///	exp ::=  nil | false | true | Number | String | `...´ | function |
+    ///		 prefixexp | tableconstructor | exp binop exp | unop exp
+    ///
+    ///	prefixexp ::= var | functioncall | `(´ exp `)´
+    ///
+    ///	functioncall ::=  prefixexp args | prefixexp `:´ Name args
+    ///
+    ///	args ::=  `(´ [explist] `)´ | tableconstructor | String
+    ///
+    ///	function ::= function funcbody
+    ///
+    ///	funcbody ::= `(´ [parlist] `)´ block end
+    ///
+    ///	parlist ::= namelist [`,´ `...´] | `...´
+    ///
+    ///	tableconstructor ::= `{´ [fieldlist] `}´
+    ///
+    ///	fieldlist ::= field {fieldsep field} [fieldsep]
+    ///
+    ///	field ::= `[´ exp `]´ `=´ exp | Name `=´ exp | exp
+    ///
+    ///	fieldsep ::= `,´ | `;´
+    ///
+    ///	binop ::= `+´ | `-´ | `*´ | `/´ | `^´ | `%´ | `..´ |
+    ///		 `<´ | `<=´ | `>´ | `>=´ | `==´ | `~=´ |
+    ///		 and | or
+    ///
+    ///	unop ::= `-´ | not | `#´
+    /// ```
     pub fn parse(&mut self) -> Result<Vec<StmtNode>> {
         self.next()?;
         let stmts = self.block()?;
