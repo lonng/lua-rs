949846ccd7981bac69379ecb6b99c5036a3a1b97
Chris Lonng <chris@lonng.org>
2018-06-20 23:37:46 +0800

    assert line info

--- _testdata/simple.lua
+++ _testdata/simple.lua
@@ -10,6 +10,17 @@ if 100 then
     print "1000000"
 end
 
+-- hello comment
+
+--[[
+    hello comment
+    hello comment
+    hello comment
+    hello comment
+    hello comment
+    hello comment
+--]]
+
 for i, v in ipairs({1,2,3,4}) do
     print(i, v)
 end
