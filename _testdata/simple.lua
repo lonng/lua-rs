3861240b6c2eaebccc14d5b54f85d868a0b0064d
Chris Lonng <chris@lonng.org>
2018-06-20 23:32:50 +0800

    fixed: expression and statement line info

--- _testdata/simple.lua
+++ _testdata/simple.lua
@@ -1,1 +1,15 @@
 print "hello"
+print "hello"
+print "hello"
+print "hello"
+print "hello"
+print "hello"
+print "hello"
+
+if 100 then
+    print "1000000"
+end
+
+for i, v in ipairs({1,2,3,4}) do
+    print(i, v)
+end
